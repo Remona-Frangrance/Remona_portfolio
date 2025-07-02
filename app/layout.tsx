b123657fbd22a5f3b031c5eab2e49100fcce3b7e
Remona Denny <dennyremona@gmail.com>
2025-07-03 00:05:18 +0530

    Inital commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Remona's Portfolio",
+  description: "I’m Remona, a dedicated MERN stack developer with a growing passion for AI-powered solutions. I enjoy turning ideas into intelligent, user-centric applications while continuously exploring the future of technology.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
