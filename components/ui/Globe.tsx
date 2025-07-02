54592e8bb1164b62111721607838110e0191598c
Remona Denny <dennyremona@gmail.com>
2025-07-03 00:15:37 +0530

    fix: deploy bugs

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -234,9 +234,11 @@ export function WebGLRendererConfig() {
   const { gl, size } = useThree();
 
   useEffect(() => {
+     if (typeof window !== "undefined") {
     gl.setPixelRatio(window.devicePixelRatio);
     gl.setSize(size.width, size.height);
     gl.setClearColor(0xffaaff, 0);
+     }
   }, []);
 
   return null;
